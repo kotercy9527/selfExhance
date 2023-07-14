41a3c4dff257112b59f52467092da7811d1d1102
kotercy <31883359+kotercy9527@users.noreply.github.com>
2023-07-15 01:23:48 +0800

    嵌套navigationcontroller

--- CollectionViewCustomLayout/AppDelegate.m
+++ CollectionViewCustomLayout/AppDelegate.m
@@ -28,9 +28,13 @@
     settingViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Setting" image:[[UIImage imageNamed:@"tab_mine_normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[[UIImage imageNamed:@"tab_mine_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
     
     UITabBar.appearance.tintColor = [UIColor redColor];//将tabbar的高亮色修改为红色
+    UITabBar.appearance.unselectedItemTintColor = [UIColor blackColor];
     
-    [tabViewController addChildViewController:homeViewController];
-    [tabViewController addChildViewController:settingViewController];
+    UINavigationController *homeNaviVC = [[UINavigationController alloc] initWithRootViewController:homeViewController];
+    UINavigationController *settingNaviVC = [[UINavigationController alloc] initWithRootViewController:settingViewController];
+    
+    [tabViewController addChildViewController:homeNaviVC];
+    [tabViewController addChildViewController:settingNaviVC];
     
     tabViewController.tabBar.backgroundColor = [UIColor whiteColor];
     
