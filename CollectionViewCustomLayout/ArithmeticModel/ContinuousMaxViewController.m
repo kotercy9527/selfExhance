484a5cefe424025b196d297ba31939cf2e598ee8
kotercy <31883359+kotercy9527@users.noreply.github.com>
2023-08-21 07:03:12 +0800

    计算连续最大的值

--- CollectionViewCustomLayout/ArithmeticModel/ContinuousMaxViewController.m
+++ CollectionViewCustomLayout/ArithmeticModel/ContinuousMaxViewController.m
@@ -21,9 +21,29 @@
     self.rawArray = @[@(2),@(3),@(4),@(-1),@(10),@(-19),@(20)];
     CGFloat value = [self maxValue:self.rawArray windowSize:4];
     NSLog(@"%f",value);
+//    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    self.rawArray = @[@(-2),@(1),@(-3),@(4),@(-1),@(2),@(1),@(-5),@(4)];
+    CGFloat maxValue = [self contiunsMaxValue];
+}
+
+- (CGFloat)contiunsMaxValue {
+    NSInteger maxValue = NSIntegerMin;
+    for (NSInteger i = 0; i < self.rawArray.count; i++) {
+        NSNumber * first = [self.rawArray  objectAtIndex:i];
+        NSInteger tmpValue = first.integerValue;
+        for (NSInteger j = i + 1; j < self.rawArray.count; j++) {
+            NSNumber *second = [self.rawArray objectAtIndex:j];
+            tmpValue += second.integerValue;
+            if (tmpValue > maxValue) {
+                maxValue = tmpValue;
+            }
+        }
+    }
+    return maxValue;
 }
 
 
+//数组连续最大值 window size是窗口大小
 - (CGFloat )maxValue:(NSArray <NSNumber*> *)arr windowSize:(NSInteger)size {
     if (size < 1) {
         return 0;
